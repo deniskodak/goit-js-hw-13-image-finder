1373dc993b1416e5781c1a6b461b16a640253fb4
Kodak, Denys <dkodak@deloitte.ua>
2023-06-28 13:08:55 +0300

    fix public path place

--- webpack/configs/production.js
+++ webpack/configs/production.js
@@ -1,24 +1,12 @@
+const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = env => ({
-  devtool: 'source-map',
   output: {
     filename: '[name].[contenthash].js',
-  },
-  publicPath: 'https://deniskodak.github.io/js-image-finder/',
-  optimization: {
-    moduleIds: 'deterministic',
-    runtimeChunk: 'single',
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-        },
-      },
-    },
+    path: path.join(__dirname, '../../dist'),
+    publicPath: 'https://deniskodak.github.io/js-image-finder/',
   },
   module: {
     rules: [
@@ -38,7 +26,7 @@ module.exports = env => ({
     }),
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css',
-      chunkFilename: '[name].[id].[contenthash].css',
+      chunkFilename: '[name].[contenthash].css',
     }),
   ],
 });
